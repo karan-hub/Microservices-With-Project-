1a0f86807cde2e4ef7f2b547210ef25ceb16cf5b
karan <karanc4143@gmail.com>
2025-09-21 16:25:52 +0530

    user service  completed

---
+++ src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.fitness.userservice.dto;
+
+import com.fitness.userservice.models.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private UserRole role;
+}
